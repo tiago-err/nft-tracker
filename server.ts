95d62705a4c28ef24892432354600e7bf98fab0e
Tiago Ribeiro <tiago.err@outlook.com>
2021-10-29 17:20:36 +0100

    Added the mongo connection with mongoose on the server.ts file

--- server.ts
+++ server.ts
@@ -1,5 +1,13 @@
+import mongoose from "mongoose";
 import app from "./index";
 const port = 8080;
+
+const {DB_URL, DB_USER, DB_PASSWORD, DB_DATABASE} = process.env;
+mongoose.connect(DB_URL as string, {auth: {username: DB_USER, password: DB_PASSWORD}, dbName: DB_DATABASE});
+
+mongoose.connection.once("open", () => {
+	console.log("MongoDB database connection established successfully");
+});
 
 console.log(`Node environment: ${process.env.NODE_ENV}`);
 app.listen(port, () => {
